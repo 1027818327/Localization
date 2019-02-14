b335063b3030532a4cfcce969ca35edc32b864fe
chenweichao <chen15270994267@gmail.com>
2019-02-14 18:12:09 +0800

    上传

---
+++ Assets/TestPro.cs
@@ -0,0 +1,77 @@
+﻿
+#region 版权信息
+/*
+ * -----------------------------------------------------------
+ *  Copyright (c) KeJun All rights reserved.
+ * -----------------------------------------------------------
+ *		描述: 
+ *      创建者：DESKTOP-1050N1H\luoyikun
+ *      创建时间: 2019/02/14 17:48:00
+ *  
+ */
+#endregion
+
+
+using UnityEngine;
+using Framework.Procedure;
+using System;
+using System.IO;
+
+namespace Assets
+{
+    public class TestPro : AbstractLoadProcedure
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Unity Messages
+        //    void Awake()
+        //    {
+        //
+        //    }
+        //    void OnEnable()
+        //    {
+        //
+        //    }
+        //
+        //    void Start() 
+        //    {
+        //    
+        //    }
+        //    
+        //    void Update() 
+        //    {
+        //    
+        //    }
+        //
+        //    void OnDisable()
+        //    {
+        //
+        //    }
+        //
+        //    void OnDestroy()
+        //    {
+        //
+        //    }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Protected & Public Methods
+
+        #endregion
+        public override void ProcedureBegin()
+        {
+            File.ReadAllText(Application.persistentDataPath + "/1.txt");
+            ProcedureEnd();
+        }
+    }
+}
